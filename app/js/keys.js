1647af43894b278fa0613f5fd42be8205a1a397f
Eduardo Lopes <EduardoLopesDS@gmail.com>
2014-08-29 03:19:38 -0300

    Space don't switch map anymore

--- app/js/keys.js
+++ app/js/keys.js
@@ -25,7 +25,7 @@
     //LEFT or A
     if(e.keyCode === 37 || e.keyCode === 65) key.left = true;
     //X or Space or L
-    if(e.keyCode === 88 || e.keyCode === 32 || e.keyCode === 76) key.sMap = true;
+    if(e.keyCode === 88 || e.keyCode === 76) key.sMap = true;
     //M
     if(e.keyCode === 77) key.m = true;
 
@@ -40,7 +40,7 @@
     if(e.keyCode === 40 || e.keyCode === 83 || e.keyCode === 90) key.down = false;
     if(e.keyCode === 39 || e.keyCode === 68) key.right = false;
     if(e.keyCode === 37 || e.keyCode === 65) key.left = false;
-    if(e.keyCode === 88 || e.keyCode === 32 || e.keyCode === 76) {
+    if(e.keyCode === 88 || e.keyCode === 76) {
       key.sMap = false;
       key.sMapPressed = false;
     };
